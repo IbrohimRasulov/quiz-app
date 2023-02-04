613b058b1495edbeec20334333e42edce4256a5c
Ibrohim Rasulov <ibrohim05062000@gmail.com>
2023-02-04 23:23:20 +0500

    Check tailwind is working, and it is working

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold text-red-500 underline text-center">Hello world!</h1>
+      <div className="text-red-600 text-3xl text-center">Hello World!</div>
+      <div className="bg-black text-white">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea, consequatur.</div>
+      <div
+        props={props}>
+        <ul>
+          <li>Lorem ipsum dolor sit amet.</li>
+          <li>Lorem ipsum dolor sit amet.</li>
+          <li>Lorem ipsum dolor sit amet.</li>
+          <li>Lorem ipsum dolor sit amet.</li>
+        </ul>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
