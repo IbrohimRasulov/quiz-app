cfd3d508a42a0bbbb5da3f30916c68307dd17fd4
Ibrohim Rasulov <ibrohim05062000@gmail.com>
2023-02-07 01:51:58 +0500

    Add assets, variables

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,10 @@
+import React from "react";
+
 const App = () => {
   return (
     <div className="App">
-      <div className="text-red-600 text-3xl text-center">Hello World!</div>
-      <div className="bg-black text-white">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea, consequatur.</div>
-      <div
-        props={props}>
-        <ul>
-          <li>Lorem ipsum dolor sit amet.</li>
-          <li>Lorem ipsum dolor sit amet.</li>
-          <li>Lorem ipsum dolor sit amet.</li>
-          <li>Lorem ipsum dolor sit amet.</li>
-        </ul>
-      </div>
+      <h1 className="text-primary">Hello world</h1>
+      <button className="">Hello</button>
     </div>
   );
 };
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#293264",
+        secondary: "#F5F7FB",
+        bg_primary: "#4D5B9E",
+        bg_select: "#D6DBF5",
+        bg_success: "#94D7A2",
+        bg_error: "#F8BCBC",
+        horizontalLine: "#DBDEF0",
+        color_disabled: "opacity: 0.5",
+      },
+      fontSize: {
+        small: "10.25px",
+        medium: "16px",
+        large: "31.25px",
+      },
+      borderRadius: {
+        small: "7.8px",
+        medium: "10px",
+        large: "15px",
+      },
+      backgroundImage: {
+        intro_background: "url('/src/assets/intro-background.png')",
+        test_background: "url('/src/assets/questions-background.png')",
+      }
+    },
   },
   plugins: [],
-}
+};
