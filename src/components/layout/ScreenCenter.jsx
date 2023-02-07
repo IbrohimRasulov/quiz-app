36084a6cb095a7e2fe38286ed43c007a0b87e05c
Ibrohim Rasulov <ibrohim05062000@gmail.com>
2023-02-07 10:17:47 +0500

    Create ScreenCenter layout component

---
+++ src/components/layout/ScreenCenter.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ScreenCenter({children}) {
+  return (
+    <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
+      {children}
+    </div>
+  )
+}
--- src/components/pages/Intro.jsx
+++ src/components/pages/Intro.jsx
@@ -1,39 +1,16 @@
 import React from "react";
 import Button from "../atoms/Button";
+import ScreenCenter from "../layout/ScreenCenter";
 
 export default function Intro() {
   return (
-    <div className="">
-      <Button
-        content="Check answers"
-        className="text-secondary bg-bg_primary disabled:opacity-70"
-        isDisabled={true}
-      />
-      <Button
-        content="Check answers"
-        className="text-secondary bg-bg_primary disabled:opacity-70"
-      />
-      <Button />
-      <Button
-        content="Check answers"
-        className="text-primary bg-bg_select"
-      />
-      <Button
-        content="Check answers"
-        className="text-primary border-btn_border"
-      />
-      <Button
-        content="Check answers"
-        className="text-primary bg-bg_success"
-      />
-      <Button
-        content="Check answers"
-        className="text-primary bg-bg_error opacity-50"
-      />
-      <Button
-        content="Check answers"
-        className="text-primary border-btn_border opacity-50"
-      />
+    <div>
+      <ScreenCenter>
+        <Button
+          content="Check answers"
+          className="text-secondary bg-bg_primary disabled:opacity-70"
+        />
+      </ScreenCenter>
     </div>
   );
 }
